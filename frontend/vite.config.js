d21991cecdf30b0cf018bd86e4a4e30c926fba7a
ahgodev <143968195+ahgodev@users.noreply.github.com>
2026-05-27 23:36:30 -0300

    fix: ajuste para deploy do front no vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
-  }
+  };
 });
